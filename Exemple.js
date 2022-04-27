839aefd3dfc61cc9c0e2c7bd92f12790025266dd
samy-alexandre <samy.alexandre85@gmail.com>
2022-04-27 19:02:16 +0200

    Mini just prix

--- Exemple.js
+++ Exemple.js
@@ -1,94 +1,58 @@
-// Tableau de citations
-let citations = [
-  [
-    "La vie est un mystère qu'il faut vivre, et non un problème à résoudre.",
-    "Gandhi",
-  ],
-  ["Le plus grand risque est de ne prendre aucun risque.", "Mark Zuckerberg"],
-  ["Méritez votre statut de leader chaque jour.", "Mickael Jordan"],
-  ["Soyez le changement que vous voulez voir dans le monde.", "Gandhi"],
-  [
-    "A chaque fois que vous vous retrouvez du même côté que la majorité, il est temps de prendre du recul, et de réfléchir.",
-    "Mark Twain",
-  ],
-  [
-    "Seulement ceux qui prendront le risque d’aller trop loin découvriront jusqu’où on peut aller.",
-    "T.S Elliot",
-  ],
-  ["Le succès c’est tomber sept fois, se relever huit.", "Proverbe japonais"],
-  [
-    "Dans vingt ans vous serez plus déçus par les choses que vous n’avez pas faites que par celles que vous avez faites. Alors sortez des sentiers battus. Mettez les voiles. Explorez. Rêvez. Découvrez.",
-    "Mark Twain",
-  ],
-  [
-    "Si vous attendez pour agir, tout ce que vous gagnerez, avec le temps, c’est de l’âge.",
-    "Brian Tracy",
-  ],
-  [
-    "Quand on concentre son attention sur un seul projet, l’esprit suggère constamment des idées et des améliorations qui lui échapperaient s’il était occupé avec plusieurs projets en même temps.",
-    "P.T. Barnum",
-  ],
-  [
-    "Se dédier à faire tout ce que l’on peut pour aider les autres à obtenir ce qu’ils veulent, c’est la clé du succès.",
-    "Brian Sher",
-  ],
-  [
-    "Si vous pensez que vous êtes trop petit pour avoir de l’impact, essayez d’aller au lit avec un moustique.",
-    "Anita Roddick",
-  ],
-  [
-    "Ne jugez pas chaque jour sur ce que vous récoltez, mais sur les graines que vous semez.",
-    "Robert Louis Stevenson",
-  ],
-  ["L’action est la clé fondamentale de tout succès.", "Pablo Picasso"],
-  [
-    "Le succès, c’est se promener d’échecs en échecs tout en restant motivé.",
-    "Winston Churchill",
-  ],
-  [
-    "Votre avenir est créé par ce que vous faîtes aujourd’hui, pas demain.",
-    "Robert T. Kiyosaki",
-  ],
-  [
-    "Ne vous découragez pas, c’est souvent la dernière clef du trousseau qui ouvre la porte.",
-    "Zig Ziglar",
-  ],
-  [
-    "Pour gagner votre vie, apprenez à l’école. Pour gagner une fortune, apprenez par vous-même.",
-    "Brian Tracy",
-  ],
-  [
-    "Les gagnants trouvent des moyens, les perdants des excuses…",
-    "F. D. Roosevelt",
-  ],
-  [
-    "Vous n’êtes jamais trop vieux pour vous fixer de nouveaux buts, ou rendre vos rêves réalité.",
-    "C.S. Lewis",
-  ],
-  [
-    "Un pessimiste voit la difficulté dans chaque opportunité. Un optimiste voit une opportunité dans chaque difficulté.",
-    "Winston Churchill",
-  ],
-];
+// Etape 1 - Sélectionner nos éléments
+let input = document.querySelector("#prix");
+let error = document.querySelector("small");
+let formulaire = document.querySelector("#formulaire");
 
-function genererNombreEntier(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+// Etape 2 - Cacher l'erreur
+error.style.display = "none";
+
+// Etape 3 - Générer un nombre aléatoire
+
+let nbAleatoire = Math.floor(Math.random() * 1001);
+let coups = 0;
+let nombreChoisi;
+
+// Etape 6 - Créer la fonction vérifier
+function verifier(nombre) {
+  let instruction = document.createElement("div");
+
+  if (nombre < nbAleatoire) {
+    instruction.textContent = "#" + coups + "(" + nombre + ") c'est plus !";
+    instruction.className = "instruction plus";
+  } else if (nombre > nbAleatoire) {
+    instruction.textContent = "#" + coups + "(" + nombre + ") c'est moins !";
+    instruction.className = "instruction moins";
+  } else {
+    instruction.textContent =
+      "#" + coups + "(" + nombre + ") Vous avez trouvé le juste prix";
+    instruction.className = "instruction fini";
+  }
+
+  // Ajouter element devant les autres
+  document.querySelector("#instructions").prepend(instruction);
 }
 
-let citation = document.querySelector("#citation");
-let auteur = document.querySelector("#auteur");
-let btn = document.querySelector("#nouveau");
+// Etape 4 - Vérifier que l'utilisateur donne bien un nombre
 
-let dernier = 0;
-let nombreAleatoire = 0;
+input.addEventListener("keyup", () => {
+  if (isNaN(input.value)) {
+    error.style.display = "inline";
+  } else {
+    error.style.display = "none";
+  }
+});
 
-btn.addEventListener("click", () => {
-  do {
-    nombreAleatoire = genererNombreEntier(citations.length);
-  } while (nombreAleatoire == dernier);
-  {
-    citation.textContent = citations[nombreAleatoire][0];
-    auteur.textContent = citations[nombreAleatoire][1];
-    dernier = nombreAleatoire;
+// Etape 5 - Agir à l'envoi du formulaire
+formulaire.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (isNaN(input.value) || input.value == "") {
+    input.style.borderColor = "red";
+  } else {
+    coups++;
+    input.style.borderColor = "gray";
+    nombreChoisi = input.value;
+    input.value = "";
+    verifier(nombreChoisi);
   }
 });
